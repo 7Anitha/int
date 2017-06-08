9b63a0ca9274e8dd238f868f3849ddb761ee008d
Anitha <anithachandran599@gmail.com>
2017-06-08 17:50:18 +0530

    Create revers.java

---
+++ revers.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class revers
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int b=0;
+while(n>0)
+{
+int a=n%10;
+b=(b*10)+a;
+n=n/10;
+}
+System.out.print(b);
+}
+}
